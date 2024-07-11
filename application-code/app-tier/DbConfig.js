bfa31718dbb2557a0756cf4702fb98c501e8a50d
gsureshkiran <g.sureshkiran@gmail.com>
2024-07-11 11:55:22 +0530

    aa

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1-instance-1.c5m0euwsijl2.ap-southeast-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Sureshkiran',
+    DB_DATABASE : 'webappdb'
+});
